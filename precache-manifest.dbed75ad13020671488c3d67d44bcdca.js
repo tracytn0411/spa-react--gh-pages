c19cf4926a438120a501c5ebd4a724cd2b357aaa
Tracy Nguyen <tracytn0411@gmail.com>
2020-03-13 14:22:58 -0700

    Updates

---
+++ precache-manifest.dbed75ad13020671488c3d67d44bcdca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f19f48023e4dbfa7ec443d57a6f929",
+    "url": "/spa-react-gh-pages/index.html"
+  },
+  {
+    "revision": "f6e17a662430a095c22c",
+    "url": "/spa-react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf783016f05cd26eae5a",
+    "url": "/spa-react-gh-pages/static/js/2.43c5fcab.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/spa-react-gh-pages/static/js/2.43c5fcab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e17a662430a095c22c",
+    "url": "/spa-react-gh-pages/static/js/main.1b057762.chunk.js"
+  },
+  {
+    "revision": "673227670602ffadca88",
+    "url": "/spa-react-gh-pages/static/js/runtime-main.9644c4e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spa-react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
